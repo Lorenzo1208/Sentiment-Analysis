b9ad09db0acd8ece02ee2cc4cf04cfc907778beb
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-04-06 17:46:47 +0200

    update

--- Home.py
+++ Home.py
@@ -8,8 +8,8 @@ import datetime
 import plotly.express as px
 
 st.title('Airline Satisfaction')
-st.write("""Airline Satisfaction""")
 
+st.image("united.jpg")
 def load_data():
     data = pd.read_csv('Airline Passenger Satisfaction.csv', delimiter=';')
     return data
