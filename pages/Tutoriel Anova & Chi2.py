905ccaf79eb66ad4ceac1cde30f95d4e28c53164
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-04-06 17:40:52 +0200

    update app

--- pages/Tutoriel Anova & Chi2.py
+++ pages/Tutoriel Anova & Chi2.py
@@ -50,3 +50,52 @@ Class_Eco_Plus	1.0	0.043316	0.043316	0.222177	6.373870e-01
 Flight_Distance	1.0	62.370654	62.370654	319.909906	1.847045e-71
 Seat_comfort	1.0	1792.009374	1792.009374	9191.527052	0.000000e+00
 Residual	129872.0	25320.258548	0.194963	NaN	NaN""")
+
+st.header("Chi2")
+st.code("""import pandas as pd
+import scipy.stats as stats
+from sklearn.preprocessing import OneHotEncoder
+
+url = "https://raw.githubusercontent.com/remijul/dataset/master/Airline%20Passenger%20Satisfaction.csv"
+df = pd.read_csv(url, delimiter=';')
+
+cat_vars = ['Gender', 'Customer Type', 'Type of Travel', 'Class']
+onehot = OneHotEncoder(sparse=False)
+cat_data = onehot.fit_transform(df[cat_vars])
+cat_df = pd.DataFrame(cat_data, columns=onehot.get_feature_names_out(cat_vars))
+
+# Convertion de la variable cible au format binaire
+df['Satisfaction_binary'] = (df['Satisfaction'] == 'satisfied').astype(int)
+
+# Pour obtenir les noms de variables encodés
+encoded_vars = onehot.get_feature_names_out(cat_vars)
+
+# Test du chi2 pour vérifier l'indépendance entre les variables catégorielles et la variable cible
+for var in cat_vars:
+    encoded_vars_for_var = [v for v in encoded_vars if v.startswith(var)]
+    for encoded_var in encoded_vars_for_var:
+        table = pd.crosstab(cat_df[encoded_var], df['Satisfaction_binary'])
+        _, p, _, _ = stats.chi2_contingency(table)
+        print(f"Chi-squared test for independence between {encoded_var} and Satisfaction:"
+        f"P-value: {p:.4f}")""")
+
+st.text("""Chi-squared test for independence between Gender_Female and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Gender_Male and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Customer Type_Loyal Customer and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Customer Type_disloyal Customer and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Type of Travel_Business travel and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Type of Travel_Personal Travel and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Class_Business and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Class_Eco and Satisfaction:
+	P-value: 0.0000
+Chi-squared test for independence between Class_Eco Plus and Satisfaction:
+	P-value: 0.0000""")
+
+
--- pages/Tutoriel Machine Learning.py
+++ pages/Tutoriel Machine Learning.py
@@ -185,7 +185,209 @@ joblib.dump(pipeline, 'best_model.joblib')
 st.header("Enregistrement du meilleur modèle et de la pipeline avec joblib")
 st.code(code6, language='python')
 
-st.header("Analyse du modèle, features importances etc sur github")
+st.header("Features importances")
+st.code("""import joblib
+
+# Load le model
+best_model = joblib.load('best_model.joblib')
+
+# Récupération du modèle RandomForest dans la pipeline
+rf_model = best_model.named_steps['classifier']
+
+# Récupération des feature importances
+feature_importances = rf_model.feature_importances_
+
+# Récupération feature names depuis one-hot encoder sauf Satisfaction
+feature_names = list(df_processed.drop('Satisfaction', axis=1).columns)
+
+# Fusion des feature importances et du noms des colonnes, puis les trier par importance
+feature_importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': feature_importances})
+feature_importance_df = feature_importance_df.sort_values('Importance', ascending=False)
+
+# Affichage du résultat
+print(feature_importance_df)""")
+
+st.text("""                              Feature  Importance
+7              Inflight_entertainment    0.190922
+2                        Seat_comfort    0.133306
+9              Ease_of_Online_booking    0.081098
+8                      Online_support    0.063117
+10                   On-board_service    0.040387
+4                      Food_and_drink    0.039299
+11                   Leg_room_service    0.033186
+1                     Flight_Distance    0.031343
+20       Customer_Type_Loyal_Customer    0.030109
+15                    Online_boarding    0.030096
+24                     Class_Business    0.029292
+0                                 Age    0.028725
+13                    Checkin_service    0.025955
+12                   Baggage_handling    0.025133
+21    Customer_Type_disloyal_Customer    0.023526
+19                        Gender_Male    0.022258
+18                      Gender_Female    0.021669
+14                        Cleanliness    0.021363
+3   Departure/Arrival_time_convenient    0.020152
+5                       Gate_location    0.018869
+23     Type_of_Travel_Personal_Travel    0.018609
+22     Type_of_Travel_Business_travel    0.017752
+17           Arrival_Delay_in_Minutes    0.015002
+16         Departure_Delay_in_Minutes    0.014405
+6               Inflight_wifi_service    0.013355
+25                          Class_Eco    0.009110
+26                     Class_Eco_Plus    0.001960""")
+
+st.header("Matrice de confusion")
+
+st.code("""from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+y_pred = best_model.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=best_model.named_steps['classifier'].classes_)
+disp.plot()""")
+
+st.image("output.png")
+
+st.header("Courbe ROC")
+
+st.code("""import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, roc_auc_score
+
+# Prédiction des probabilités pour l'ensemble de test
+y_prob = best_model.predict_proba(X_test)[:, 1]
+
+# Calcul du taux de faux positifs (FPR), du taux de vrais positifs (TPR) et du score ROC AUC.
+fpr, tpr, _ = roc_curve(y_test, y_prob)
+roc_auc = roc_auc_score(y_test, y_prob)
+
+# Plot du ROC
+plt.figure()
+plt.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc="lower right")
+plt.show()""")
+
+st.image("output2.png")
+
+st.header("Cross validation")
+
+st.code("""from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(best_model, X_train, y_train, cv=5, scoring='accuracy')
+print("Scores cross-validation :", scores)
+print("Moyenne score cross-validation :", scores.mean())
+print("Écart-type des scores validés par croisement :", scores.std())""")
+
+st.text("""Scores cross-validation : [0.95573897 0.95776619 0.95607684 0.95622164 0.95708838]
+Moyenne score cross-validation : 0.956578406104658
+Écart-type des scores validés par croisement : 0.0007422146085809153""")
+
+st.header("Courbe d'apprentissage")
+
+st.code("""from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+
+train_sizes, train_scores, test_scores = learning_curve(best_model, X_train, y_train, cv=5)
+
+plt.plot(train_sizes, train_scores.mean(axis=1), 'o-', label='Training score')
+plt.plot(train_sizes, test_scores.mean(axis=1), 'o-', label='Cross-validation score')
+plt.xlabel('Training set size')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()""")
+
+st.image("output3.png")
+
+st.header("Prédiction sur de nouvelles données")
+
+st.code("""import joblib
+import pandas as pd
+
+# Charger le modèle entraîné
+best_model = joblib.load('best_model.joblib')
+
+# Créer un DataFrame avec de nouvelles données
+df = pd.DataFrame({
+    'Gender': ['Female'],
+    'Customer Type': ['Loyal Customer'],
+    'Age': [30],
+    'Type of Travel': ['Business travel'],
+    'Class': ['Business'],
+    'Flight Distance': [2000],
+    'Seat comfort': [5],
+    'Departure/Arrival time convenient': [5],
+    'Food and drink': [4],
+    'Gate location': [3],
+    'Inflight wifi service': [4],
+    'Inflight entertainment': [4],
+    'Online support': [4],
+    'Ease of Online booking': [5],
+    'On-board service': [4],
+    'Leg room service': [4],
+    'Baggage handling': [4],
+    'Checkin service': [4],
+    'Cleanliness': [4],
+    'Online boarding': [4],
+    'Departure Delay in Minutes': [10],
+    'Arrival Delay in Minutes': [20]
+}, index=[0])
+
+# Convertir les colonnes catégorielles en type 'category'
+categorical_col = ['Gender', 'Customer Type', 'Type of Travel', 'Class']
+df[categorical_col] = df[categorical_col].astype('category')
+
+# Faire des prédictions avec le modèle
+predictions = best_model.predict(df)
+print(predictions)""")
+st.text("""[1] Ici le client sera satisfait d'après le modèle """)
+
+st.code("""import joblib
+import pandas as pd
+
+# Charger le modèle entraîné
+best_model = joblib.load('best_model.joblib')
+
+# Créer un DataFrame avec de nouvelles données
+df = pd.DataFrame({
+    'Gender': ['Female'],
+    'Customer Type': ['Loyal Customer'],
+    'Age': [30],
+    'Type of Travel': ['Business travel'],
+    'Class': ['Business'],
+    'Flight Distance': [2000],
+    'Seat comfort': [1],
+    'Departure/Arrival time convenient': [1],
+    'Food and drink': [1],
+    'Gate location': [1],
+    'Inflight wifi service': [1],
+    'Inflight entertainment': [0],
+    'Online support': [1],
+    'Ease of Online booking': [1],
+    'On-board service': [1],
+    'Leg room service': [1],
+    'Baggage handling': [1],
+    'Checkin service': [1],
+    'Cleanliness': [1],
+    'Online boarding': [1],
+    'Departure Delay in Minutes': [10],
+    'Arrival Delay in Minutes': [20]
+}, index=[0])
+
+# Convertir les colonnes catégorielles en type 'category'
+categorical_col = ['Gender', 'Customer Type', 'Type of Travel', 'Class']
+df[categorical_col] = df[categorical_col].astype('category')
+
+# Faire des prédictions avec le modèle
+predictions = best_model.predict(df)
+print(predictions)""")
+
+st.text("""[0] Ici le client ne sera pas satisfait d'après le modèle """)
+
+st.header("Notebook Github")
 import webbrowser
 
 if st.button('Accéder au code sur GitHub'):
